0ab61b9fc1e24472999e67ec32c714c6c00389bb
dlovestec <chaudharydharmesh32@gmail.com>
2022-02-08 00:33:59 +1100

    Test User Seeder Added.

---
+++ database/seeders/TestUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TestUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
